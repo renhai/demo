5a4120dbc77ae5c690577ad7a7abe9fb919c4947
renhai <renhai@meituan.com>
2015-09-14 10:22:58 +0800

    happy number and rotate image

--- src/main/java/com/sankuai/meituan/test/AndyTest.java
+++ src/main/java/com/sankuai/meituan/test/AndyTest.java
@@ -1,5 +1,7 @@
 package com.sankuai.meituan.test;
 
+import java.text.DecimalFormat;
+import java.util.LinkedList;
 import java.util.Stack;
 
 
@@ -17,7 +19,23 @@ public class AndyTest {
     	Integer item = stack.pop();
     	System.out.println(item);
     	System.out.println(stack.isEmpty());
-    	System.out.println(stack.pop());
+//    	System.out.println(stack.pop());
+    	
+    	LinkedList<Integer> ll = new LinkedList<Integer>();
+    	ll.add(11);
+    	ll.add(12);
+    	while (!ll.isEmpty()) {
+			System.out.print(ll.poll() + "\t");
+		}
+    	System.out.println();
+    	
+//    	DecimalFormat df = new DecimalFormat("00");
+//    	for (int j = 1; j <= 50; j++) {
+//    		System.out.println("INSERT INTO `meituanpoiop`.`zb_user_exchange` (`user_id`, `user_name`, `invite_code`) VALUES ('"+-j+"', '美团高校"+df.format(j)+"', '美团高校"+df.format(j)+"');");
+//		}
+    	
+    	System.out.println(1L << 31);
+    	
 	}
 	
 }
