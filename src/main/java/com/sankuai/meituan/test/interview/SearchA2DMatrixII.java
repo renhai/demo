db61b9b6d06eb801e1b57bac1a4a05b63d9ae280
andy <myrenhai@gmail.com>
2015-12-14 16:26:28 -0800

    Search A 2D Matrix II

---
+++ src/main/java/com/sankuai/meituan/test/interview/SearchA2DMatrixII.java
@@ -0,0 +1,28 @@
+package com.sankuai.meituan.test.interview;
+
+public class SearchA2DMatrixII {
+
+	public static void main(String[] args) {
+		
+	}
+	
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0) {
+        	return false;
+        }
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int m = 0, n = cols - 1;
+        while (m < rows && n > -1) {
+        	if (matrix[m][n] == target) {
+        		return true;
+        	} else if (matrix[m][n] > target) {
+        		n --;
+        	} else {
+        		m ++;
+        	}
+        }
+        return false;
+    } 
+
+}
