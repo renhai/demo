d0c652738abc9a4434a991d4cc4c0f559a489d55
andy <myrenhai@gmail.com>
2015-12-15 16:05:24 -0800

    reverse linked list

---
+++ src/main/java/com/sankuai/meituan/test/interview/ReverseLinkedList.java
@@ -0,0 +1,22 @@
+package com.sankuai.meituan.test.interview;
+
+public class ReverseLinkedList {
+
+	public static void main(String[] args) {
+
+	}
+	
+    public ListNode reverseList(ListNode head) {
+    	ListNode curr = head;
+    	ListNode prev = null;
+    	ListNode next = null;
+    	while (curr != null) {
+    		next = curr.next;
+    		curr.next = prev;
+    		prev = curr;
+    		curr = next;
+    	}
+    	return prev;
+    }
+
+}
