c56499567a4ce3ea9f6dc18c2bc13087c1988d9f
andy <myrenhai@gmail.com>
2015-12-17 14:04:04 -0800

    ReverseLinkedListII

---
+++ src/main/java/com/sankuai/meituan/test/interview/ReverseLinkedListII.java
@@ -0,0 +1,30 @@
+package com.sankuai.meituan.test.interview;
+
+public class ReverseLinkedListII {
+
+	public static void main(String[] args) {
+
+	}
+	
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+    	ListNode dummy = new ListNode(0);
+    	dummy.next = head;
+    	
+    	ListNode prev = dummy;
+    	ListNode curr = null;
+    	ListNode next = null;
+    	for (int i = 0; i < m - 1; i++) {
+			prev = prev.next;
+		}
+    	curr = prev.next;
+    	next = curr.next;
+    	for (int i = 0; i < n - m; i++) {
+			curr.next = next.next;
+			next.next = prev.next;
+			prev.next = next;
+			next = curr.next;
+		}
+    	return dummy.next;
+    }
+
+}
