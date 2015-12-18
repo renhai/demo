419276aa3a223f5c1123463be306dde3bd271dbc
andy <myrenhai@gmail.com>
2015-12-18 11:22:27 -0800

    MergeIntervals

---
+++ src/main/java/com/sankuai/meituan/test/interview/Interval.java
@@ -0,0 +1,16 @@
+package com.sankuai.meituan.test.interview;
+
+public class Interval {
+	int start;
+	int end;
+
+	Interval() {
+		start = 0;
+		end = 0;
+	}
+
+	Interval(int s, int e) {
+		start = s;
+		end = e;
+	}
+}
