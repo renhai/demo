283fff9f6db5aeeb5d2d938827229da5ef614b9d
andy <myrenhai@gmail.com>
2015-12-17 18:56:00 -0800

    MergeSortedArray

--- src/main/java/com/sankuai/meituan/test/interview/MergeSortedArray.java
+++ src/main/java/com/sankuai/meituan/test/interview/MergeSortedArray.java
@@ -19,14 +19,20 @@ public class MergeSortedArray {
 	 * @param nums2
 	 * @param n
 	 */
-	//TODO
-	public void merge(int[] A, int m, int[] B, int n) {
-		int i = m - 1, j = n - 1, k = m + n - 1;
-		while (i > -1 && j > -1)
-			A[k--] = (A[i] > B[j]) ? A[i--] : B[j--];
-		while (j > -1)
-			A[k--] = B[j--];
-
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+		int i = m - 1;
+		int j = n - 1;
+		int k = m + n - 1;
+		while (i >= 0 && j >=0) {
+			if (nums1[i] > nums2[j]) {
+				nums1[k --] = nums1[i --];
+			} else {
+				nums1[k --] = nums2[j --];
+			}
+		}
+		while (j >= 0) {
+			nums1[k --] = nums2[j--];
+		}
 	}
 
 }
