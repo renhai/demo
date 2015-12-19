ad90c06fac2f649dbd834f9831d3c8502901b62b
andy <myrenhai@gmail.com>
2015-12-18 16:46:56 -0800

    BalancedBinaryTree

---
+++ src/main/java/com/sankuai/meituan/test/interview/BalancedBinaryTree.java
@@ -0,0 +1,24 @@
+package com.sankuai.meituan.test.interview;
+
+/**
+ * https://leetcode.com/problems/balanced-binary-tree/
+ * @author andy
+ *
+ */
+public class BalancedBinaryTree {
+
+	public static void main(String[] args) {
+
+	}
+	
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+        return isBalanced(root.left) && isBalanced(root.right);
+    }
+    
+    private int height (TreeNode root) {
+    	if (root == null) return 0;
+    	return Math.max(height(root.right), height(root.left)) + 1;
+    }
+
+}
