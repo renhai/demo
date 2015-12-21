20e436b86f98e4b10bcafa59d7f33eb055728c30
andy <myrenhai@gmail.com>
2015-12-21 15:19:21 -0800

    LongestSubstringWithoutRepeatingCharacters

--- src/main/java/com/sankuai/meituan/test/interview/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/com/sankuai/meituan/test/interview/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,5 +1,8 @@
 package com.sankuai.meituan.test.interview;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/longest-substring-without-repeating-characters/
  * Given a string, find the length of the longest substring without repeating characters. 
@@ -11,11 +14,23 @@ package com.sankuai.meituan.test.interview;
 public class LongestSubstringWithoutRepeatingCharacters {
 
 	public static void main(String[] args) {
-
+		String a = "abba";
+		System.out.println(new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstring(a));
 	}
 	
     public int lengthOfLongestSubstring(String s) {
-        return 0;
+    	Map<Character, Integer> map = new HashMap<>();
+    	int start = 0;
+    	int maxlen = 0;
+    	for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			if (map.containsKey(c)) {
+				start = Math.max(map.get(c) + 1, start);//pay more attention
+			} 
+			maxlen = Math.max(i - start + 1, maxlen);
+			map.put(c, i);
+		}
+        return maxlen;
     }
 
 }
