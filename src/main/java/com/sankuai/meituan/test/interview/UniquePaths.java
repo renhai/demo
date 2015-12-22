49a3e51233f866970bc4839240b3c83b6999bca3
andy <myrenhai@gmail.com>
2015-12-22 13:24:17 -0800

    DB UniquePaths

--- src/main/java/com/sankuai/meituan/test/interview/UniquePaths.java
+++ src/main/java/com/sankuai/meituan/test/interview/UniquePaths.java
@@ -8,11 +8,11 @@ package com.sankuai.meituan.test.interview;
 public class UniquePaths {
 
 	public static void main(String[] args) {
-
+		System.out.println(new UniquePaths().uniquePaths(3, 7));
 	}
 	
 	// 排列组合问题 https://leetcode.com/discuss/47829/math-solution-o-1-space
-    public int uniquePaths(int m, int n) {
+    public int uniquePaths2(int m, int n) {
     	if (m == 1 || n == 1) {
     		return 1;
     	}
@@ -30,6 +30,27 @@ public class UniquePaths {
 			res /= j;
 		}
     	return (int) res;
+    }
+    
+    public int uniquePaths(int m, int n) {
+    	if (m == 1 || n == 1) {
+    		return 1;
+    	}
+    	
+    	int[][] arr = new int[m][n];
+    	for (int i = 0; i < m; i ++) {
+    		arr[i][0] = 1;
+    	}
+    	for (int i = 0; i < n; i ++) {
+    		arr[0][i] = 1;
+    	}
+    	
+    	for (int i = 1; i < m; i ++) {
+    		for (int j = 1; j < n; j++) {
+				arr[i][j] = arr[i][j - 1] + arr[i - 1][j];
+			}
+    	}
+    	return arr[m - 1][n - 1];
     }
 
 }
