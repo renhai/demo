e6bf5b08390f9d4098b6e424b98ec62c9186fbbb
andy <myrenhai@gmail.com>
2015-12-22 12:09:04 -0800

    UniquePaths

---
+++ src/main/java/com/sankuai/meituan/test/interview/UniquePaths.java
@@ -0,0 +1,34 @@
+package com.sankuai.meituan.test.interview;
+
+/**
+ * https://leetcode.com/problems/unique-paths/
+ * @author andy
+ *
+ */
+public class UniquePaths {
+
+	public static void main(String[] args) {
+
+	}
+	
+    public int uniquePaths(int m, int n) {
+    	if (m == 1 || n == 1) {
+    		return 1;
+    	}
+    	m --;
+    	n --;
+    	
+    	if (m < n) {
+	    	m = m + n;
+	    	n = m - n;
+	    	m = m - n;
+    	}
+    	long res = 1;
+    	for (int i = m + 1,j = 1;  i <= m + n ; i++, j ++) {
+			res *= i;
+			res /= j;
+		}
+    	return (int) res;
+    }
+
+}
