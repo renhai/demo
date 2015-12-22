7cf9ba428b59eeffc3324729ec8f569bc7fa5cb0
andy <myrenhai@gmail.com>
2015-12-22 11:00:24 -0800

    ValidateBinarySearchTree

---
+++ src/main/java/com/sankuai/meituan/test/interview/ValidateBinarySearchTree.java
@@ -0,0 +1,36 @@
+package com.sankuai.meituan.test.interview;
+
+import java.util.Stack;
+
+/**
+ * https://leetcode.com/problems/validate-binary-search-tree/
+ * @author andy
+ *
+ */
+public class ValidateBinarySearchTree {
+
+	public static void main(String[] args) {
+
+	}
+	
+    public boolean isValidBST(TreeNode root) {
+    	if (root == null) return false;
+    	Stack<TreeNode> stack = new Stack<>();
+    	TreeNode curr = root;
+    	TreeNode pre = null;
+    	while (curr != null || !stack.isEmpty()) {
+    		while (curr != null) {
+    			stack.push(curr);
+    			curr = curr.left;
+    		}
+    		TreeNode node = stack.pop();
+    		if (pre != null && pre.val > node.val) {
+    			return false;
+    		} 
+    		pre = node;
+    		curr = node.right;
+    	}
+    	return true;
+    }
+
+}
