852c95734491ed91b64c946198f2c2fa7d107c40
andy <myrenhai@gmail.com>
2015-12-28 14:47:51 -0800

    MaximumSubarray

---
+++ src/main/java/com/sankuai/meituan/test/interview/MaximumSubarray.java
@@ -0,0 +1,33 @@
+package com.sankuai.meituan.test.interview;
+
+/**
+ * https://leetcode.com/problems/maximum-subarray/
+ * @author andy
+ *
+ */
+public class MaximumSubarray {
+
+	public static void main(String[] args) {
+		int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+		int res = new MaximumSubarray().maxSubArray(nums);
+		System.out.println(res);
+	}
+	
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+        	return 0;
+        }
+        int max = nums[0];
+        int sum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+        	if (sum < 0) {
+        		sum = nums[i];
+        	} else {
+        		sum += nums[i];
+        	}
+        	max = Math.max(max, sum);
+		}
+        return max;
+    }
+
+}
