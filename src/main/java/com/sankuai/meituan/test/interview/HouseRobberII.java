48117ae803ca2673d1ecacefe6065b44bf82fb6f
andy <myrenhai@gmail.com>
2015-12-28 17:14:04 -0800

    modify HouseRobberII

--- src/main/java/com/sankuai/meituan/test/interview/HouseRobberII.java
+++ src/main/java/com/sankuai/meituan/test/interview/HouseRobberII.java
@@ -16,17 +16,17 @@ public class HouseRobberII {
         if (nums.length == 1) return nums[0];
         if (nums.length == 2) return Math.max(nums[0], nums[1]);
         if (nums.length == 3) return Math.max(Math.max(nums[0], nums[1]), nums[2]);
-        int[] res1 = new int[nums.length];
-        int[] res2 = new int[nums.length - 1];
-        res1[0] = nums[0]; res1[1] = Math.max(nums[0], nums[1]);
-        res2[0] = nums[1]; res2[1] = Math.max(nums[1], nums[2]);
+        int[] resFromFirst = new int[nums.length];
+        int[] resFromSecond = new int[nums.length];
+        resFromFirst[0] = nums[0]; 
+        resFromFirst[1] = Math.max(nums[0], nums[1]);
+        resFromSecond[0] = 0; 
+        resFromSecond[1] = nums[1];
         for (int i = 2; i < nums.length; i++) {
-        	res1[i] = Math.max(nums[i] + res1[i - 2], res1[i - 1]);
+        	resFromFirst[i] = Math.max(nums[i] + resFromFirst[i - 2], resFromFirst[i - 1]);
+        	resFromSecond[i] = Math.max(nums[i] + resFromSecond[i - 2], resFromSecond[i - 1]);
 		}
-        for (int i = 3; i < nums.length; i++) {
-        	res2[i - 1] = Math.max(nums[i] + res2[i - 3], res2[i - 2]);
-		}
-        return Math.max(res1[res1.length - 2], res2[res2.length - 1]);
+        return Math.max(resFromFirst[resFromFirst.length - 2], resFromSecond[resFromSecond.length - 1]);
     }
 
 }
